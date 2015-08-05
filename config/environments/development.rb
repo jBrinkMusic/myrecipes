971f37f6b76bef2b976f25bdd87028d98bfded39
jBrinkMusic <jonathanbrinkmusic@gmail.com>
2015-08-05 08:05:07 +0000

    Create new and edit forms and partials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  # Whitelisted IP addresses
+  config.web_console.whitelisted_ips = '194.157.173.25'
 end
